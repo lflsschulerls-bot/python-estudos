de88535ea38e8c581b6aef6ce81abc8608abb5fd
leo_schuler2 <lfls.schuler.ls@gmail.com>
2026-05-29 01:56:39 -0300

    Feature: Codei com sucesso o copiarRenomear

--- ProjetoRelatorio/copiarRenomear.py
+++ ProjetoRelatorio/copiarRenomear.py
@@ -1,6 +1,16 @@
-import shutil, datetime
+import shutil, os, datetime
 
-arquivoWord = r"C:\Users\usuario\OneDrive - Universidade Federal de Pernambuco\FACULDADE\Relatórios PIBID 2024-26\MODELORelatório_PIBID_LEONARDO_FERNANDO_DE_LEMOS_SCHULER.docx"
-pastaDestino = r"C:\Users\usuario\OneDrive - Universidade Federal de Pernambuco\FACULDADE\Relatórios PIBID 2024-26\2026"
+arquivoOriginal = r"C:\Users\usuario\OneDrive - Universidade Federal de Pernambuco\FACULDADE\Relatórios PIBID 2024-26\MODELORelatório_PIBID_LEONARDO_FERNANDO_DE_LEMOS_SCHULER.docx"
+copia = r"C:\Users\usuario\OneDrive - Universidade Federal de Pernambuco\FACULDADE\Relatórios PIBID 2024-26\2026\relatorio_copia.docx"
 
-shutil.copy(arquivoWord, pastaDestino)
+shutil.copy(arquivoOriginal, copia)
+print('Arquivo copiado com sucesso!')
+
+# Renomear o arquivo
+dataHoje = datetime.datetime.now()
+mesHoje = dataHoje.strftime("%m")
+anoHoje = dataHoje.strftime("%Y")
+nomeArquivo = f"Relatório({"%m"}.{anoHoje})_PIBID_LEONARDO_FERNANDO_DE_LEMOS_SCHULER.docx"
+renomeado = os.path.join(r"C:\Users\usuario\OneDrive - Universidade Federal de Pernambuco\FACULDADE\Relatórios PIBID 2024-26\2026", nomeArquivo)
+os.rename(copia, renomeado)
+print('Arquivo renomeado com sucesso!')
