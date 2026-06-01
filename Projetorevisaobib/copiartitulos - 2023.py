c3baa7fb31a1a3edbf4d26b8dfd9518e8f49c41d
leo_schuler2 <lfls.schuler.ls@gmail.com>
2026-06-01 10:46:27 -0300

    Feature: programei a colheita dos dados de 5 anos consecutivos

---
+++ Projetorevisaobib/copiartitulos - 2023.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests, bs4
+
+res = requests.get('https://repositorio.ufpe.br/handle/123456789/25413/simple-search?location=123456789%2F25413&query=&filter_field_1=dateIssued&filter_type_1=equals&filter_value_1=2023&rpp=40&sort_by=dc.date.issued_dt&order=DESC&etal=0&submit_search=Atualizar')
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text)
+
+# A lista é uma tabela, encontre <tr> a árvore principal
+linhas = soup.find_all('tr')
+
+listaDissertacoes = []
+
+# Para cada linha da tabela, buscar título e autor
+for linha in linhas:
+    celulasTitulos = linha.find('td', headers='t2')
+    celulaAutores = linha.find('td', headers='t3')
+
+# Guardar título e autor
+    if celulasTitulos and celulaAutores:
+        tituloTexto = celulasTitulos.text.strip()
+        autor = celulaAutores.text.strip()
+        dissertacao = {"titulo": tituloTexto, 
+                       "autor": autor,
+                       "ano": 2023
+        }
+        listaDissertacoes.append(dissertacao)
+
+print(f"Total de dissertações coletadas: {len(listaDissertacoes)}")
+print(listaDissertacoes)
+
+# Armazenar na planilha do excel
+df = pd.DataFrame(listaDissertacoes)
+df.to_excel('dissertacoes_2023.xlsx', index=False)
+print(f"Pronto! {len(df)} dissertações foram salvas na planilha 'dissertacoes_2023.xlsx'.")
--- Projetorevisaobib/copiartitulos - 2025.py
+++ Projetorevisaobib/copiartitulos - 2025.py
@@ -1,18 +1,21 @@
+import pandas as pd
 import requests, bs4
 
 res = requests.get('https://repositorio.ufpe.br/handle/123456789/25413/simple-search?location=123456789%2F25413&query=&filter_field_1=dateIssued&filter_type_1=equals&filter_value_1=2025&rpp=40&sort_by=dc.date.issued_dt&order=DESC&etal=0&submit_search=Atualizar')
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text)
 
-# A lista é uma tabela, encontre <td> e headers="t2" para título e headers='t3' para autor
+# A lista é uma tabela, encontre <tr> a árvore principal
 linhas = soup.find_all('tr')
 
 listaDissertacoes = []
 
+# Para cada linha da tabela, buscar título e autor
 for linha in linhas:
-    celulasTitulos = soup.find('td', headers='t2')
-    celulaAutores = soup.find('td', headers='t3')
+    celulasTitulos = linha.find('td', headers='t2')
+    celulaAutores = linha.find('td', headers='t3')
 
+# Guardar título e autor
     if celulasTitulos and celulaAutores:
         tituloTexto = celulasTitulos.text.strip()
         autor = celulaAutores.text.strip()
@@ -24,3 +27,8 @@ for linha in linhas:
 
 print(f"Total de dissertações coletadas: {len(listaDissertacoes)}")
 print(listaDissertacoes)
+
+# Armazenar na planilha do excel
+df = pd.DataFrame(listaDissertacoes)
+df.to_excel('dissertacoes_2025.xlsx', index=False)
+print(f"Pronto! {len(df)} dissertações foram salvas na planilha 'dissertacoes_2025.xlsx'.")
